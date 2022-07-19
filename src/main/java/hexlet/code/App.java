0d6222a285252bc4f058d4c8ab25d836222274c7
Wo0ty <pautov.e@yandex.ru>
2022-07-19 23:58:58 +0300

    Add javalin and SLF4j

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,26 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Javalin app = getApp();
+        app.start(getPort());
     }
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            config.enableDevLogging();
+        });
+
+        app.get("/", ctx -> ctx.result("Hello World"));
+
+        return app;
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "5000");
+        return Integer.parseInt(port);
+    }
+
 }
